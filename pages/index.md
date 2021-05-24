0fe4178f73a40bed6e31aec9ad4df2df187d8d25
stuncloud <stuncloud+github@gmail.com>
2021-05-24 17:35:35 +0900

    :new: github pages
    設定ファイルのjson schemaを公開

---
+++ pages/index.md
@@ -0,0 +1,4 @@
+UWSCR
+=====
+
+enjoy!
---
+++ src/evaluator/builtins/array_control.rs
@@ -0,0 +1,9 @@
+use crate::evaluator::object::*;
+use crate::evaluator::builtins::*;
+use crate::evaluator::UError;
+
+
+pub fn builtin_func_sets() -> BuiltinFunctionSets {
+    let mut sets = BuiltinFunctionSets::new();
+    sets
+}
