4e9a9687fab5595f16b5f699ad521b6a6e89e552
stuncloud <stuncloud+github@gmail.com>
2022-12-26 15:00:47 +0900

    バージョン更新 0.9.1

--- documents/source/conf.py
+++ documents/source/conf.py
@@ -9,7 +9,7 @@
 project = 'UWSCR'
 copyright = '2022, stuncloud'
 author = 'stuncloud'
-version = '0.9.0'
+version = '0.9.1'
 html_title = f'{project} {version}'
 
 # -- General configuration ---------------------------------------------------
