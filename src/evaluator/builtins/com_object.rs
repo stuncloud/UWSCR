22f961adff03762adf06efbec23b58a183393cdb
stuncloud <stuncloud+github@gmail.com>
2023-10-19 16:28:55 +0900

    oleeventを実行したらUnavailableFunctionエラーにする

--- src/evaluator/builtins/com_object.rs
+++ src/evaluator/builtins/com_object.rs
@@ -13,7 +13,7 @@ pub fn builtin_func_sets() -> BuiltinFunctionSets {
     sets.add("createoleobj", 1, createoleobj);
     sets.add("getactiveoleobj", 3, getactiveoleobj);
     sets.add("getoleitem", 1, getoleitem);
-    // sets.add("oleevent", 4, oleevent);
+    sets.add("oleevent", 4, oleevent);
     sets.add("vartype", 2, vartype);
     sets.add("safearray", 4, safearray);
     sets.add("xlopen", 36, xlopen);
@@ -116,34 +116,35 @@ pub fn getoleitem(_: &mut Evaluator, args: BuiltinFuncArgs) -> BuiltinFuncResult
     Ok(Object::Array(vec))
 }
 
-// pub fn oleevent(evaluator: &mut Evaluator, args: BuiltinFuncArgs) -> BuiltinFuncResult {
-//     let mut com = args.get_as_comobject(0)?;
-//     if args.len() > 1 {
-//         // セット
-//         let interface = args.get_as_string(1, None)?;
-//         let event = args.get_as_string(2, None)?;
-//         let func = match args.get_as_function_or_string(3, true)? {
-//             Some(two) => match two {
-//                 TwoTypeArg::T(name) => {
-//                     match evaluator.env.get_function(&name) {
-//                         Some(obj) => match obj {
-//                             Object::Function(func) => Ok(func),
-//                             _ => Err(builtin_func_error(UErrorMessage::IsNotUserFunction(name))),
-//                         },
-//                         None => Err(builtin_func_error(UErrorMessage::FunctionNotFound(name))),
-//                     }
-//                 },
-//                 TwoTypeArg::U(func) => Ok(func),
-//             },
-//             None => Err(builtin_func_error(UErrorMessage::FunctionRequired)),
-//         }?;
-//         com.set_event_handler(&interface, &event, func, evaluator.clone())?;
-//     } else {
-//         // 解除
-//         com.remove_event_handler()?;
-//     }
-//     Ok(Object::Empty)
-// }
+pub fn oleevent(_evaluator: &mut Evaluator, _args: BuiltinFuncArgs) -> BuiltinFuncResult {
+    // let mut com = args.get_as_comobject(0)?;
+    // if args.len() > 1 {
+    //     // セット
+    //     let interface = args.get_as_string(1, None)?;
+    //     let event = args.get_as_string(2, None)?;
+    //     let func = match args.get_as_function_or_string(3, true)? {
+    //         Some(two) => match two {
+    //             TwoTypeArg::T(name) => {
+    //                 match evaluator.env.get_function(&name) {
+    //                     Some(obj) => match obj {
+    //                         Object::Function(func) => Ok(func),
+    //                         _ => Err(builtin_func_error(UErrorMessage::IsNotUserFunction(name))),
+    //                     },
+    //                     None => Err(builtin_func_error(UErrorMessage::FunctionNotFound(name))),
+    //                 }
+    //             },
+    //             TwoTypeArg::U(func) => Ok(func),
+    //         },
+    //         None => Err(builtin_func_error(UErrorMessage::FunctionRequired)),
+    //     }?;
+    //     com.set_event_handler(&interface, &event, func, evaluator.clone())?;
+    // } else {
+    //     // 解除
+    //     com.remove_event_handler()?;
+    // }
+    // Ok(Object::Empty)
+    Err(builtin_func_error(UErrorMessage::UnavailableFunction))
+}
 
 fn vartype(_: &mut Evaluator, args: BuiltinFuncArgs) -> BuiltinFuncResult {
     let obj = args.get_as_object(0, None)?;
