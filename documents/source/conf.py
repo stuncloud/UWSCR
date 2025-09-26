e06b09980f16662cfbab5bc1f15c0661f0040184
stuncloud <stuncloud+github@gmail.com>
2025-09-26 13:10:16 +0900

    バージョン更新

--- documents/source/conf.py
+++ documents/source/conf.py
@@ -9,7 +9,7 @@
 project = 'UWSCR'
 copyright = '2025, stuncloud'
 author = 'stuncloud'
-version = '1.1.4'
+version = '1.1.5'
 html_title = f'{project} {version}'
 
 # -- General configuration ---------------------------------------------------
